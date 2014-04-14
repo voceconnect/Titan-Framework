b62834834f444e25ea487439c83b5795bab99c05
Kevin Langley <me@kevinlangleyjr.com>
2014-04-14 18:59:03 -0400

    switching multicheck-posts to use WP_Query and allow extra query args to be passed in

--- class-option-multicheck-posts.php
+++ class-option-multicheck-posts.php
@@ -11,28 +11,36 @@ class TitanFrameworkOptionMulticheckPosts extends TitanFrameworkOptionMulticheck
 		'post_status' => 'any',
 		'orderby' => 'post_date',
 		'order' => 'DESC',
+		'extra_query_args' => array()
 	);
 
 	/*
 	 * Display for options and meta
 	 */
 	public function display() {
-		$args = array(
+		$query_args = array(
 			'post_type' => $this->settings['post_type'],
 			'posts_per_page' => $this->settings['num'],
 			'post_status' => $this->settings['post_status'],
 			'orderby' => $this->settings['orderby'],
 			'order' => $this->settings['order'],
+			'fields' => 'ids'
 		);
 
-		$posts = get_posts( $args );
-
-		$this->settings['options'] = array();
-		foreach ( $posts as $post ) {
-			$this->settings['options'][$post->ID] = $post->post_title;
+		if( !empty( $this->settings['extra_query_args'] ) ){
+			$query_args = array_merge( $query_args, $this->settings['extra_query_args'] );
 		}
 
-		parent::display();
+		$posts_query = new WP_Query( $query_args );
+
+		if( !empty( $posts_query->posts ) ){
+			$this->settings['options'] = array();
+			foreach( $posts_query->posts as $post_id ){
+				$this->settings['options'][$post_id] = the_title_attribute( array( 'post' => $post_id, 'echo' => false ) );
+			}
+
+			parent::display();
+		}
 	}
 
 	/*
